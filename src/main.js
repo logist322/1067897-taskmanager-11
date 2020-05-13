549eb927f4115e4c25326886aca9ed3161f2621d
silentPartner <hytornoy-99@mail.ru>
2020-05-13 23:57:18 +0300

    Done

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import MenuComponent, {MenuItem} from './components/site-menu.js';
 import FilterController from './controllers/filter.js';
 import BoardController from './controllers/board.js';
 import BoardComponent from './components/board.js';
+import StatisticsComponent from "./components/statistics.js";
 import TasksModel from './models/tasks.js';
 import {generateTasks} from './mock/tasks.js';
 import {render} from './utils/render.js';
@@ -28,11 +29,32 @@ render(siteMainElement, boardComponent);
 const boardController = new BoardController(boardComponent, tasksModel);
 boardController.render();
 
+const dateTo = new Date();
+const dateFrom = (() => {
+  return new Date(dateTo).setDate(dateTo.getDate() - 7);
+})();
+
+const statisticsComponent = new StatisticsComponent({tasks: tasksModel, dateFrom, dateTo});
+render(siteMainElement, statisticsComponent);
+statisticsComponent.hide();
+
 menuComponent.setOnChange((menuItem) => {
   switch (menuItem) {
     case MenuItem.NEW_TASK:
       menuComponent.setActiveItem(MenuItem.TASKS);
+      statisticsComponent.hide();
+      boardController.show();
       boardController.createTask();
+      break;
+
+    case MenuItem.TASKS:
+      statisticsComponent.hide();
+      boardController.show();
+      break;
+
+    case MenuItem.STATISTICS:
+      boardController.hide();
+      statisticsComponent.show();
       break;
   }
 });
